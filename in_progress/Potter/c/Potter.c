b10250ebc3a374e8e2d341228090759631d5ffe5
Aaylor <loloruna@orange.fr>
2012-10-25 23:27:45 +0200

    have to be fix

--- in_progress/Potter/c/Potter.c
+++ in_progress/Potter/c/Potter.c
@@ -2,40 +2,68 @@
 #include <stdlib.h>
 #include "Potter.h"
 
-/*
- *  GOOD TO KNOW :
- *  1 book = 8 EUR
- *  2 different books = 15,2 EUR
- *  3 different books = 21,6 EUR
- *  4 different books = 25,6 EUR
- *  5 different books = 30 EUR
- *
- */
+#define ONE_BOOK_PRICE      8.0
+#define TWO_BOOKS_PRICES    15.2
+#define THREE_BOOKS_PRICES  21.6
+#define FOUR_BOOKS_PRICES   25.6
+#define FIVE_BOOKS_PRICES   30.0
 
-int main(){
+int main()
+{
+    run_test();
+}
 
+
+void run_test()
+{
     int numberOfBooks[5];
     fillArray(numberOfBooks, 5);
     displayArray(numberOfBooks, 5);
 
+    printf("Number of books : %d.\n", getNumberBooks(numberOfBooks, 5));
 }
 
-void fillArray(int *array, int arrayLength){
 
+void fillArray(int *array, int arrayLength)
+{
     int i;
-    for (i = 0; i < arrayLength; i++){
+    for (i = 0; i < arrayLength; i++)
+    {
         printf("Number of Vol. %d : ", i+1);
         scanf("%d", &array[i]); 
     }
-
 }
 
-void displayArray(int *array, int arrayLength){
 
+void displayArray(int *array, int arrayLength)
+{
     int i;
-    for (i = 0; i < arrayLength; i++){
+    for (i = 0; i < arrayLength; i++)
+    {
         printf("Vol. %d :: %d\n", i+1, *(array+i));
     }
+}
+
+
+int getNumberBooks(int *array, int arrayLength, int &maxBook)
+{
+    int number = 0;
+    int i;
+    
+    for (i = 0; i < arrayLength; i++)
+    {
+        number += *(array+i);
+        if ( *maxBook < *(array+i) )
+            *maxBook = *(array+i);
+    }
+
+    return number;
+}
+
+int simpleAlgo(int *array, int arrayLength)
+{
+    
+    return 1;
 
 }
 
--- in_progress/Potter/c/Potter.h
+++ in_progress/Potter/c/Potter.h
@@ -1,7 +1,9 @@
 #ifndef POTTER_H_INCLUDED
 #define POTTER_H_INCLUDED
 
+void run_test();
 void fillArray(int*, int);
 void displayArray(int*, int);
+int getCost(int*, int);
 
 #endif
